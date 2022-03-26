e6ce1933566778fefe1cf821b03b75188d3fbdb1
Sneepex2 <mteonidis@gmail.com>
2022-03-26 23:22:38 +0100

    Favorites Commit

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorites-context';
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ function MainNavigation() {
             <Link to='/meetups/new-meetup'>New Meetup</Link>
           </li>
           <li>
-            <Link to='/meetups/favorites'>My Favorites</Link>
+            <Link to='/meetups/favorites'>
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+              My Favorites
+            </Link>
           </li>
         </ul>
       </nav>
